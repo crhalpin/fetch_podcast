312bdb232f96dbcef03f6a35fbcebd83b547b2da
Corey Halpin <chalpin@cs.wisc.edu>
2014-09-27 16:28:49 -0500

    Add --cache option to skip fetching and add copious comments.

--- src/fetch_podcast/core.clj
+++ src/fetch_podcast/core.clj
@@ -9,32 +9,37 @@
   (:gen-class))
 
 (defn do_homedir
-  "Expand ~/ to current user's HOME"
+  "Expand ~/ to current user's HOME."
   [fname]
   (str/replace fname #"^~/" (str (System/getenv "HOME") "/" )) )
 
+(defn file_exists
+  "Determine if a file exists."
+  [fname]
+  (.exists (io/as-file fname)))
+
 (defn read_pref
-  "Read a preference file, if it exists"
+  "Read a preference file, if it exists."
   [fname & [default reset] ]
   (let [file_path (do_homedir (str "~/.fetch_podcast/" fname))]
-    (if (or reset (not (.exists (io/as-file file_path))))
+    (if (or reset (not (file_exists file_path)))
       default
       (read-string (slurp file_path)))))
 
 (defn save_pref
-  "Save a preference file"
+  "Save a preference file."
   [fname data]
   (spit (do_homedir (str "~/.fetch_podcast/" fname)) data) )
 
 (defn cache_fname
-  "Get the cache file name for a given feed"
+  "Get the cache file name for a given feed."
   [feed]
   (do_homedir (str "~/.fetch_podcast/cache/" (feed :title))))
 
 (defn copy
-  "Copy a URI to a file, if the target did not exist"
+  "Copy a URI to a file, if the target did not exist."
   [uri file]
-  (if (not (.exists (io/as-file file)))
+  (if (not (file_exists file))
     (with-open [in (io/input-stream uri)
                 out (io/output-stream file)]
       (io/copy in out))))
@@ -52,7 +57,7 @@
 (defn get_key [x] (keyword (sha256 x)))
 
 (defn parse_item
-  "Parse an XML item to produce a map describing the feed"
+  "Parse an XML item to produce a map describing the feed."
   [xdata]
   (loop [rc {} elem xdata]
     ; If there are no more elements in this <item>, we're done
@@ -72,7 +77,7 @@
            (recur rc tl))))))
 
 (defn parse_feed
-  "Parse an XML feed, returning an array of maps describing the enclosures"
+  "Parse an XML feed, returning an array of maps describing the enclosures."
   [xdata]
   ; Feeds start out as <rss><channel>, so grab the content of the channel
   (loop [rc [] elem ( (first (xdata :content)) :content ) ]
@@ -88,106 +93,140 @@
           (recur rc tl))))))
 
 (defn fetch_feed
-  "Fetch an XML feed with caching and If-None-Match/If-Modified-Since"
+  "Fetch an XML feed with caching and If-None-Match/If-Modified-Since."
   [feed cache options]
-  (let [verbosity (options :verbosity)]
-    (let [url (feed :feed)
-          last_resp (cache (get_key url))
-          headers
-          (cond
-           (nil? last_resp) {}
-           (and (contains? last_resp "Expires")
-                (pos? (compare (new java.util.Date (last_resp "Expires"))
-                               (new java.util.Date))))
-             nil
-           (contains? last_resp "Etag")
-             {"If-None-Match" (last_resp "Etag") }
-           (contains? last_resp "ETag")
-             {"If-None-Match" (last_resp "ETag") }
-           (contains? last_resp "Last-Modified")
-             {"If-Modified-Since" (last_resp "Last-Modified")} )]
-      (if (> verbosity 1) (do (println (str "Updating " (feed :title) " from " url ))))
-      (if (nil? headers)
-        (do (if (> verbosity 1) (println "\tNot expired, using cache"))
-            nil)
-        (let [http_resp (http/get url {:headers headers :throw-exceptions false}) ]
-          (cond
-           (= (http_resp :status) 304)
-             (do (if (> verbosity 1) (println "\tGot 304, using cache"))
-                 nil)
-           (= (http_resp :status) 200)
-             (do
-               (if (> verbosity 1) (println "\tFetching"))
-               (spit (cache_fname feed) (http_resp :body))
-               { (get_key url) (http_resp :headers) } )
-           :else
-             (throw (str "Error loading " url))))))))
+  (let [verbosity (options :verbosity) ]
+    (if (options :cache)
+      nil ; If we're only using cached feeds, skip fetching new ones.
+
+      (let [url (feed :feed)
+            last_resp (cache (get_key url))
+            ; Decide what additional headers shoudl be included when
+            ; we fetch this feed, based on the http response from the
+            ; last time that we fetched it.
+            headers
+            (cond
+             ; On our initial fetch, don't use any additional headers
+             (nil? last_resp) {}
+
+             ; If our cached data is still good, set the headers to
+             ; nil to indicate that we should skip the fetch.
+             (and (contains? last_resp "Expires")
+                  (pos? (compare (new java.util.Date (last_resp "Expires"))
+                                 (new java.util.Date))))
+               nil
+
+             ; If we have an Etag, ETag, or Last-Modified, then use
+             ; that on our next fetch.  Prefer the Etag to the
+             ; Last-Modified in the case that we have both.
+             (contains? last_resp "Etag")
+               {"If-None-Match" (last_resp "Etag") }
+             (contains? last_resp "ETag")
+               {"If-None-Match" (last_resp "ETag") }
+             (contains? last_resp "Last-Modified")
+               {"If-Modified-Since" (last_resp "Last-Modified")} )]
+
+        (if (> verbosity 1)
+          (do (println (str "Updating " (feed :title) " from " url ))))
+
+        (if (nil? headers)
+          (do (if (> verbosity 1)
+                (println "\tNot expired, using cache"))
+              nil)
+
+          ; If we need a new fetch, then do one.
+          (let [http_resp (http/get url {:headers headers :throw-exceptions false}) ]
+            (cond
+             ; If the response is 304, use cached data.
+             (= (http_resp :status) 304)
+               (do (if (> verbosity 1)
+                     (println "\tGot 304, using cache"))
+                   nil)
+
+             ; If the response was 200, stick the new data in the cache.
+             (= (http_resp :status) 200)
+               (do (if (> verbosity 1)
+                     (println "\tFetching"))
+                   (spit (cache_fname feed) (http_resp :body))
+                   { (get_key url) (http_resp :headers) } )
+
+             :else
+               (throw (str "Error loading " url)))))))))
 
 (defn process_feed
-  "Process a specified feed, downloading enclosures if required"
+  "Process a specified feed, downloading enclosures as required."
   [feed done tgts options]
   (let [verbosity (options :verbosity)
-        catchup (options :catchup)
-        dry-run (options :dry-run)
-        skip (or catchup dry-run)
-        episodes (options :episodes)
-        ep_tgts (if episodes tgts nil)
-        fd_tgts (if (and (not episodes)
-                         (not (empty? tgts))) tgts nil) ]
-    (loop [new_items #{}
-           items (parse_feed (xml/parse (cache_fname feed))) ]
+        ep_tgts (if (options :episodes) tgts nil)
+        fd_tgts (if (and (not (options :episodes))
+                         (not (empty? tgts))) tgts nil)
+        file (cache_fname feed)]
 
-      ; If we have no more items, or if there is a list of feeds that
-      ; we're not on, then we're done
-      (if (or (empty? items)
-              (and (not (nil? fd_tgts))
-                   (not (contains? tgts (feed :title))) ))
-        new_items
+    (if (not (file_exists file))
+      (do (if (> verbosity 0)
+            (println (str "Skipping " (feed :title) " because "
+                          "cache file (" file ") is missing")))
+        #{})
 
-        (let [hd (first items) tl (rest items)
-              hv (sha256 (hd :enclosure))]
-          ; If we've already gotten this ep, or if there was a list of eps
-          ;  that this is not on, then proceed to the next item
-          (if (or (contains? done (keyword hv))
-                  (and (not (nil? ep_tgts))
-                       (not (contains? tgts hv ))))
-            (recur new_items tl)
+      ; Iterate over the items in this feed.
+      (loop [new_items #{}
+             items (parse_feed (xml/parse file)) ]
 
-            (let [url (hd :enclosure)
-                  ftgt (do_homedir
-                        (str (feed :path) "/"
-                             ((eval (feed :name_fn)) hd)) ) ]
-              (do
-                (if (> verbosity 0) (println (str hv "\n\t" url) ))
-                (if (not skip) (copy url ftgt) )
-                (if (> verbosity 0)  (println (str "\t-> " ftgt)))
-                (if dry-run
-                  (recur new_items tl)
-                  (recur (conj new_items
-                               (get_key url)) tl))))))))))
+        ; If we have no more items, or if there is a list of feeds that
+        ; we're not on, then we're done
+        (if (or (empty? items)
+                (and (not (nil? fd_tgts))
+                     (not (contains? tgts (feed :title))) ))
+          new_items
+
+          (let [hd (first items) tl (rest items)
+                hv (sha256 (hd :enclosure))]
+
+            ; If we've already gotten this ep, or if there was a list of eps
+            ;  that this is not on, then proceed to the next item
+            (if (or (contains? done (keyword hv))
+                    (and (not (nil? ep_tgts))
+                         (not (contains? tgts hv ))))
+              (recur new_items tl)
+
+              (let [url (hd :enclosure)
+                    ; Figure out the target filename by calling the
+                    ; name_fn from the feed config.
+                    ftgt (do_homedir
+                          (str (feed :path) "/"
+                               ((eval (feed :name_fn)) hd)) ) ]
+                (do
+                  (if (> verbosity 0) (println (str hv "\n\t" url) ))
+                  (if (not (or (options :catchup)
+                               (options :dry-run)))
+                    (copy url ftgt))
+                  (if (> verbosity 0)  (println (str "\t-> " ftgt)))
+                  (if (options :dry-run)
+                    (recur new_items tl)
+                    (recur (conj new_items
+                                 (get_key url)) tl)))))))))))
 
 (defn -main [& args]
   ; Parse arguments
-  (let [cli-options  [["-v" nil "Verbosity"
-                       :id :verbosity
-                       :default 0
-                       :assoc-fn (fn [m k _] (update-in m [k] inc)) ]
-                      ["-c" "--catchup"]
-                      ["-d" "--dry-run"]
-                      ["-i" "--init"]
-                      ["-e" "--episodes"]
-                      ["-F" "--force-fetch"] ]
+  (let [cli-options [["-v" nil "Verbosity"
+                      :id :verbosity
+                      :default 0
+                      :assoc-fn (fn [m k _] (update-in m [k] inc)) ]
+                     ["-c" "--catchup"]
+                     ["-d" "--dry-run"]
+                     ["-i" "--init"]
+                     ["-e" "--episodes"]
+                     ["-F" "--force-fetch"]
+                     ["-C" "--cache"] ]
         opt_map (parse-opts args cli-options)
         options (opt_map :options)
-        reinit  (options :init)
-        refetch (options :force-fetch)
         tgts    (into #{} (opt_map :arguments)) ]
 
     ;TODO: Print a warning for unknown feeds
     (loop
       [feeds (read_pref "feeds.clj" [])
-       cache (read_pref "cache_metadata.clj" {} refetch)
-       done  (read_pref "fetchlog.clj" #{} reinit ) ]
+       cache (read_pref "cache_metadata.clj" {} (options :force-fetch) )
+       done  (read_pref "fetchlog.clj" #{} (options :init) ) ]
 
     ; If there were no feeds left to fetch, we're done.
     ; Save a fetchlog and exit
